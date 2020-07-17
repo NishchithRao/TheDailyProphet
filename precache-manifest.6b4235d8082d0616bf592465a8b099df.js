b580d704c288c123991e07f7c6e9873c5453c094
NishchithRao <devmonk4@gmail.com>
2020-07-17 11:16:31 +0530

    Updates

---
+++ precache-manifest.6b4235d8082d0616bf592465a8b099df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f50590eb1e7cbf551bcfbe42348ce8c",
+    "url": "/TheDailyProphet/index.html"
+  },
+  {
+    "revision": "29fa6dc9cf57c28911af",
+    "url": "/TheDailyProphet/static/css/main.7ccaa944.chunk.css"
+  },
+  {
+    "revision": "b2f4c05f79c5dff1ede0",
+    "url": "/TheDailyProphet/static/js/2.7d784c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheDailyProphet/static/js/2.7d784c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fa6dc9cf57c28911af",
+    "url": "/TheDailyProphet/static/js/main.99faf328.chunk.js"
+  },
+  {
+    "revision": "9dcc97007a3f5d11b6df",
+    "url": "/TheDailyProphet/static/js/runtime-main.bd60b383.js"
+  }
+]);
